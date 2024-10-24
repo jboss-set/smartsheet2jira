9adea653ef185fc722eda728acba2e9590ea41da
Carlo de Wolf <cdewolf@redhat.com>
2024-10-24 21:53:45 +0200

    Use configurable transformers

--- sync-fix-version/src/main/java/org/jboss/set/smartsheet2jira/syncfixversion/SyncFixVersions.java
+++ sync-fix-version/src/main/java/org/jboss/set/smartsheet2jira/syncfixversion/SyncFixVersions.java
@@ -21,14 +21,20 @@
  */
 package org.jboss.set.smartsheet2jira.syncfixversion;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.smartsheet.api.Smartsheet;
 import com.smartsheet.api.SmartsheetException;
 import com.smartsheet.api.SmartsheetFactory;
 import com.smartsheet.api.models.Cell;
 import com.smartsheet.api.models.Column;
+import com.smartsheet.api.models.PagedResult;
 import com.smartsheet.api.models.Row;
 import com.smartsheet.api.models.Sheet;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -38,39 +44,50 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SyncFixVersions {
-    private static final Map<String, Column> columnMap = new HashMap<>();
-    private static final Pattern pattern = Pattern.compile("CP GA EAP 8\\.0\\.(\\d[\\.\\d]*).*");
-    private static final String format = "8.0 Update %s";
-
-    public static void main(final String[] args) throws SmartsheetException {
+    public static void main(final String[] args) throws SmartsheetException, IOException {
         final String accessToken = args[0];
+        System.out.println(new File(".").getAbsoluteFile());
+        final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        mapper.findAndRegisterModules();
+        // TODO: properly find default-transformers.yaml
+        final List<Transformer> transformers = mapper.readValue(new File("sync-fix-version/src/main/resources/default-transformers.yaml"), new TypeReference<List<Transformer>>(){});
         final Smartsheet smartsheet = SmartsheetFactory.createDefaultClient(accessToken);
-        /*
+        final Map<String, Sheet> sheetsMap = new HashMap<>();
         final PagedResult<Sheet> sheets = smartsheet.sheetResources().listSheets();
         for (Sheet sheet : sheets.getData()) {
+            sheetsMap.put(sheet.getName(), sheet);
+        }
+        for (Transformer transformer : transformers) {
+            final String sheetName = transformer.getSheetName();
+            final Sheet s = sheetsMap.get(sheetName);
+            if (s == null) {
+                System.err.println("Unable to find sheet " + sheetName);
+                continue;
+            }
+            // now truly load the sheet
+            final Sheet sheet = smartsheet.sheetResources().getSheet(s.getId());
             System.out.println(sheet.getName());
-            System.out.println("> " + sheet.getId());
-        }
-        */
-        final Sheet sheet = smartsheet.sheetResources().getSheet(8602266509830020L); // should be EAP 8.0.z. v10 _Latest version
-        System.out.println(sheet.getName());
-        for (Column column : sheet.getColumns()) {
-            columnMap.put(column.getTitle(), column);
-        }
-        //System.out.println(columnMap.get("Finish").getType()); // ABSTRACT_DATETIME
-        final List<Row> rows = sheet.getRows();
-        for (final Row row : rows) {
-            final List<Cell> cells = row.getCells();
-            final String taskName = Optional.ofNullable(cells.get(columnMap.get("Task Name").getIndex()).getValue()).orElse("").toString();
-            final Matcher m = pattern.matcher(taskName);
-            if (!m.matches()) continue;
-            final String finish = Optional.ofNullable(cells.get(columnMap.get("Finish").getIndex()).getValue()).orElse("").toString();
-            final String done = Optional.ofNullable(cells.get(columnMap.get("Done checkbox").getIndex()).getValue()).orElse("").toString();
-            //System.out.println(taskName);
-            final List<String> groups = new ArrayList<>();
-            for (int i = 1; i <= m.groupCount(); i++)
-                groups.add(m.group(i));
-            System.out.println(String.format(format, groups.toArray()) + " - " + finish + " - " + done);
+            final Map<String, Column> columnMap = new HashMap<>();
+            for (Column column : sheet.getColumns()) {
+                columnMap.put(column.getTitle(), column);
+            }
+            final Pattern pattern = Pattern.compile(transformer.getTaskPattern());
+            final String format = transformer.getFixVersionFormat();
+            final List<Row> rows = sheet.getRows();
+            for (final Row row : rows) {
+                final List<Cell> cells = row.getCells();
+                final String taskName = Optional.ofNullable(cells.get(columnMap.get("Task Name").getIndex()).getValue()).orElse("").toString();
+                final Matcher m = pattern.matcher(taskName);
+                if (!m.matches()) continue;
+                final String finish = Optional.ofNullable(cells.get(columnMap.get("Finish").getIndex()).getValue()).orElse("").toString();
+                final String done = Optional.ofNullable(cells.get(columnMap.get("Done checkbox").getIndex()).getValue()).orElse("").toString();
+                //System.out.println(taskName);
+                final List<String> groups = new ArrayList<>();
+                for (int i = 1; i <= m.groupCount(); i++)
+                    groups.add(m.group(i));
+                System.out.println(String.format(format, groups.toArray()) + " - " + finish + " - " + done);
+            }
+            //System.out.println(columnMap.get("Finish").getType()); // ABSTRACT_DATETIME
         }
     }
 }

47a3e3a8f29fbc99db87eea909121241eaf81ee4
Carlo de Wolf <cdewolf@redhat.com>
2024-10-28 13:52:08 +0100

    Added logic to process the smartsheet to update fix versions

--- sync-fix-version/src/main/java/org/jboss/set/smartsheet2jira/syncfixversion/SyncFixVersions.java
+++ sync-fix-version/src/main/java/org/jboss/set/smartsheet2jira/syncfixversion/SyncFixVersions.java
@@ -21,6 +21,11 @@
  */
 package org.jboss.set.smartsheet2jira.syncfixversion;
 
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.JiraRestClientFactory;
+import com.atlassian.jira.rest.client.api.domain.Version;
+import com.atlassian.jira.rest.client.api.domain.input.VersionInputBuilder;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
@@ -32,19 +37,25 @@ import com.smartsheet.api.models.Column;
 import com.smartsheet.api.models.PagedResult;
 import com.smartsheet.api.models.Row;
 import com.smartsheet.api.models.Sheet;
+import org.jboss.set.aphrodite.issue.trackers.jira.auth.BearerHttpAuthenticationHandler;
+import org.joda.time.DateTime;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SyncFixVersions {
-    public static void main(final String[] args) throws SmartsheetException, IOException {
+    public static void main(final String[] args) throws SmartsheetException, IOException, URISyntaxException {
         System.out.println(new File(".").getAbsoluteFile());
 
         final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
@@ -59,6 +70,18 @@ public class SyncFixVersions {
             sheetsMap.put(sheet.getName(), sheet);
         }
 
+        final Map<String, Version> fixVersions = new HashMap<>();
+        final JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+        final URI uri = new URI("https://issues.redhat.com");
+        final JiraRestClient jira = factory.createWithAuthenticationHandler(uri, new BearerHttpAuthenticationHandler(secrets.get("jiraAccessToken")));
+        // TODO: make project configurable
+        for (Version version : jira.getProjectClient().getProject("JBEAP").claim().getVersions()) {
+            //System.out.println(version.getName() + " - " + version.getReleaseDate());
+            fixVersions.put(version.getName(), version);
+        }
+
+        // To deal with duplicate entries we keep track
+        final Set<String> processed = new HashSet<>();
         for (Transformer transformer : transformers) {
             final String sheetName = transformer.getSheetName();
             final Sheet s = sheetsMap.get(sheetName);
@@ -81,13 +104,43 @@ public class SyncFixVersions {
                 final String taskName = Optional.ofNullable(cells.get(columnMap.get("Task Name").getIndex()).getValue()).orElse("").toString();
                 final Matcher m = pattern.matcher(taskName);
                 if (!m.matches()) continue;
+                // already did such an entry
+                if (processed.contains(taskName)) continue;
                 final String finish = Optional.ofNullable(cells.get(columnMap.get("Finish").getIndex()).getValue()).orElse("").toString();
-                final String done = Optional.ofNullable(cells.get(columnMap.get("Done checkbox").getIndex()).getValue()).orElse("").toString();
+                final DateTime finishDateTime = DateTime.parse(finish).withTimeAtStartOfDay();
+                final boolean done = (Boolean) Optional.ofNullable(cells.get(columnMap.get("Done checkbox").getIndex()).getValue()).orElse(false);
                 //System.out.println(taskName);
                 final List<String> groups = new ArrayList<>();
                 for (int i = 1; i <= m.groupCount(); i++)
                     groups.add(m.group(i));
-                System.out.println(String.format(format, groups.toArray()) + " - " + finish + " - " + done);
+                final String fixVersion = String.format(format, groups.toArray());
+                final Version version = fixVersions.get(fixVersion); // can be null if it doesn't exist yet
+                if (version == null) {
+                    System.out.println("NYI NEW " + fixVersion + " - " + finishDateTime + " - " + done);
+                } else {
+                    final VersionInputBuilder vib = new VersionInputBuilder("JBEAP", version);
+                    boolean changed = false;
+                    System.out.print(fixVersion + " - ");
+                    if (finishDateTime.equals(version.getReleaseDate())) {
+                        System.out.print(finishDateTime);
+                    } else {
+                        System.out.print(finishDateTime + ">" + version.getReleaseDate());
+                        vib.setReleaseDate(finishDateTime);
+                        changed = true;
+                    }
+                    System.out.print(" - ");
+                    if (done == version.isReleased()) {
+                        System.out.println(done);
+                    } else {
+                        System.out.println(done + ">" + version.isReleased());
+                        vib.setReleased(done);
+                        changed = true;
+                    }
+                    if (changed) {
+                        jira.getVersionRestClient().updateVersion(version.getSelf(), vib.build()).claim();
+                    }
+                }
+                processed.add(taskName);
             }
             //System.out.println(columnMap.get("Finish").getType()); // ABSTRACT_DATETIME
         }
